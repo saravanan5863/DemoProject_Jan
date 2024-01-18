30c5dd95894293be936f523de4fe9ac1376f0e4c
saravanan5863 <saravananarunachalam1231@gmail.com>
2024-01-18 17:28:29 +0530

    Initial Project Setup

---
+++ src/main/java/org/pageobjects/LoginPage.java
@@ -0,0 +1,12 @@
+package org.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	public LoginPage(WebDriver  driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+}
